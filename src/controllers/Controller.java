325dfd822ab441126ca2c8e1268a49bf113d1b18
pdgfaustin <pdgfaustin@gmail.com>
2022-01-17 11:53:40 +0100

    Creation du Controller abstrait qui sera hériter par ceux parlant du modèle CRUD. l'interface de création des utilisateurs non rempli a aussi été créé

---
+++ src/controllers/Controller.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+/**
+ *
+ * @author user
+ */
+public abstract class Controller {
+    public abstract void enregistrer();
+    public abstract void modifier();
+    public abstract void supprimer();
+}
